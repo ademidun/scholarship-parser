af86d37b1204321fd9162a4305b34ba2e3ab0bac
Evan Farrell <evan.farrell0@gmail.com>
2018-03-31 14:06:32 -0400

    parse and parse utils

---
+++ scholparser/parserapp/parse_utils.py
@@ -0,0 +1,25 @@
+def get_keywords(html):
+    keywords = {}
+
+
+    get_keyword('deadline')
+
+    keywords = get_deadline(html, keywords)
+    keywords = get_schools(html, keywords)
+    keywords = get_locations(html, keywords)
+
+
+def get_deadline(html, keywords):
+    keywords = {}
+
+    return keywords
+
+def get_schools(html, keywords):
+    keywords = {}
+
+    return keywords
+
+def get_locations(html, keywords):
+    keywords = {}
+
+    return keywords
